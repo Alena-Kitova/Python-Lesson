19ddf98887a4aeec368cfb2b8c5e26c910a8c4e6
Alena-Kitova <akitova808@gmail.com>
2025-10-27 02:52:03 +0900

    lesson5

--- Lesson5/lesson05_task2.py
+++ Lesson5/lesson05_task2.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(
     service=ChromeService(ChromeDriverManager().install()))
@@ -11,10 +10,8 @@ driver = webdriver.Chrome(
 driver.get("http://uitestingplayground.com/dynamicid")
 sleep(5)
 
-blue_button = driver.find_element(By.CSS_SELECTOR, 'button.btn-primary')
-blue_button.send_keys("Button with Dynamic ID")
-sleep(5)
+driver.find_element(By.LINK_TEXT, "Button with Dynamic ID").click()
 
-blue_button.send_keys(Keys.RETURN)
+sleep(5)
 
 driver.quit()
